de9fd18d6693c1a04fde22c9fd31ceda58fc8ced
willdebras <wtb3na@virginia.edu>
2020-07-30 15:37:16 -0500

    updated sharla1 palette to the defaults from ggkeyboard

--- R/sharla_colors.R
+++ R/sharla_colors.R
@@ -1,5 +1,5 @@
 colorlist <- list(
-  sharla1 = c(keyboard_color = "#51504A",  modifier_color = "#ffce00", alphanum_color = "#EDEDD8", accent_color = "#454A49", background_colour = "lightgrey", alphanum_stroke = "#99ccf1", modifier_stroke = "#59aae6", accent_stroke = "#85dab1", text_color = "lightgrey"),
+  sharla1 = c(keyboard_color = "#fbbcb8",  modifier_color = "#78baeb", alphanum_color = "#bfdff6", accent_color = "#a3e3c4", background_colour = "lightgrey", alphanum_stroke = "#99ccf1", modifier_stroke = "#59aae6", accent_stroke = "#85dab1", text_color = "#5F5F5F"),
   sharla2 = c(keyboard_color = "#E5E7EB",  modifier_color = "#155E90", alphanum_color = "#DFDED9", accent_color = "#F9B668", background_colour = "lightgrey", alphanum_stroke = "#cbc9c1", modifier_stroke = "#135582", accent_stroke = "#f8a546", text_color = "#f8a546"),
   sharla3 = c(keyboard_color = "#313131",  modifier_color = "#FF4893", alphanum_color = "#6F4CA4", accent_color = "#00A8E8", background_colour = "lightgrey", alphanum_stroke = "#644494", modifier_stroke = "#ff2d81", accent_stroke = "#0096d1", text_color = "white")
 )
