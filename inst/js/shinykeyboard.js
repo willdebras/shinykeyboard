64160750ecae0e65b6d6fd08272c843af82f1ad3
Maya Gans <jaffe.maya@gmail.com>
2020-07-31 07:10:41 -0700

    click OR use keyboard

--- inst/js/shinykeyboard.js
+++ inst/js/shinykeyboard.js
@@ -15,28 +15,46 @@ $.extend(shinyKeyboardBinding, {
     return value
   },
 
-  // on keyboard press add selected to the svg element with
-  // the id corresponding to the pressed key
-  subscribe: function(el, callback) {
-    $(document.body).keydown(function (evt) {
-      // remove any selected classes
+  // on key click add "selected" class
+  // to svg path with the id of the pressed key
+  subscribe: function (el, callback) {
+    $(el).on("click.shinyKeyboardBinding", function (evt) {
+      // remove all of the selected classes inside our element
       $(el).find(".selected").removeClass("selected");
+      // set the selected class to the closest clicked part
+      console.log($(evt.target).attr('id'))
+      $(evt.target).addClass('selected');
+      callback()
+    });
+    $(document).keyup(function (evt) {
 
+      // 1. GET KEYCODE ARRAY
+      pressedKeys = [];
       // while this records multiple keys (YAY)
       // I don't know how to store them as an array
-      let key_down = String.fromCharCode(evt.keyCode).toLowerCase();
-      console.log(key_down)
+      var key = pressedKeys[evt.keyCode]
+      console.log(pressedKeys)
 
-      // once I can return multiple
-      // I need to figure out how to add selected class
-      // to all elements
-      // for now
-      // this just returns the last key pressed, not multiple
+      // 2. CONVERT KEYCODE TO KEY NAME
+      // eventually this needs to return from a look up table
+      // to include for non-alphabetic keys
+      // but lets get this working for multiple letters first
+      let key_down = String.fromCharCode(evt.keyCode).toLowerCase()
+
+      // 3. ADD SELECTED CLASS TO ALL ELEMENTS IN ARRAY
+      // add class "selected" to all elements
+      // in the pressedKeys vector
       $("#" + key_down).addClass('selected');
-      callback();
+      callback(true)
+    });
+    // 4. USE KEYDOWN TO CLEAR ARRAY
+    //    AND CLEAR SELECTED CLASSES?
+    $(document).keydown(function (evt) {
+      $(el).find(".selected").removeClass("selected");
+      callback(true)
     });
   },
-  unsubscribe: function(el) {
+  unsubscribe: function (el) {
     $(el).off(".shinyKeyboardBinding");
   }
 });
