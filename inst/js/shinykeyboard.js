d3f4e757f19a25fc70af3f1c0a2b23571c5d77db
Maya Gans <jaffe.maya@gmail.com>
2020-08-02 20:24:47 -0700

    adding correct object structure

--- inst/js/shinykeyboard.js
+++ inst/js/shinykeyboard.js
@@ -1,71 +1,107 @@
-let keymap = [
-  {backspace: 8},
-  {tab: 9},
-  {enter:	13},
-  {shift:	16},
-  {ctrl:	17},
-  {alt:	18},
-  {pause:	19},
-  {'caps lock': 20},
-  {escape: 27},
-  {'page up':	33},
-  {'page down':	34},
-  {end:	35},
-  {home: 36},
-  {'left arrow': 37},
-  {'up arrow':	38},
-  {'right arrow':	39},
-  {'down arrow':	40},
-  {insert:	45},
-  {delete: 46},
-  {0:	48},
-  {1:	49},
-  {2:	50},
-  {3:	51},
-  {4:	52},
-  {5:	53},
-  {6:	54},
-  {7:	55},
-  {8:	56},
-  {9:	57},
-  {a:	65},
-  {b:	66},
-  {c:	67},
-  {d:	68},
-  {e:	69},
-  {f:	70},
-  {g:	71},
-  {h:	72},
-  {i:	73},
-  {j:	74},
-  {k:	75},
-  {l:	76},
-  {m:	77},
-  {n:	78},
-  {o:	79},
-  {p:	80},
-  {q:	81},
-  {r:	82},
-  {s:	83},
-  {t:	84},
-  {u:	85},
-  {v:	86},
-  {w:	87},
-  {x:	88},
-  {y:	89},
-  {z:	90},
-  {"left window key": 91},
-  {"right window key": 92},
-  {"select key":	93},
-  {"numpad 0":	96},
-  {"numpad 1":	97},
-  {"numpad 2":	98},
-  {"numpad 3":	99},
-  {"numpad 4":	100},
-  {"numpad 5":	101},
-  {"numpad 6":	102},
-  {"numpad 7":	103}
-]
+let keymap = {
+  'backspace': 8,
+  'tab': 9,
+  'enter': 13,
+  'shift': 16,
+  'ctrl': 17,
+  'alt': 18,
+  'pause': 19,
+  'caps lock': 20,
+  'escape': 27,
+  'page up': 33,
+  'page down': 34,
+  'end': 35,
+  'home': 36,
+  'left arrow': 37,
+  'up arrow':	38,
+  'right arrow':	39,
+  'down arrow':	40,
+  'insert':	45,
+  'delete': 46,
+  '0': 48,
+  '1': 49,
+  '2': 50,
+  '3': 51,
+  '4': 52,
+  '5': 53,
+  '6': 54,
+  '7': 55,
+  '8': 56,
+  '9': 57,
+  'a': 65,
+  'b': 66,
+  'c': 67,
+  'd': 68,
+  'e': 69,
+  'f': 70,
+  'g': 71,
+  'h': 72,
+  'i': 73,
+  'j': 74,
+  'k': 75,
+  'l': 76,
+  'm': 77,
+  'n': 78,
+  'o': 79,
+  'p': 80,
+  'q': 81,
+  'r': 82,
+  's': 83,
+  't': 84,
+  'u': 85,
+  'v': 86,
+  'w': 87,
+  'x': 88,
+  'y': 89,
+  'z': 90,
+  'left window key': 91,
+  'right window key': 92,
+  'select key':	93,
+  'numpad 0':	96,
+  'numpad 1':	97,
+  'numpad 2':	98,
+  'numpad 3':	99,
+  'numpad 4':	100,
+  'numpad 5':	101,
+  'numpad 6':	102,
+  'numpad 7':	103,
+  'numpad 8':	104,
+  'numpad 9': 105,
+  'multiply': 106,
+  'add':	107,
+  'subtract':	109,
+  'decimal point': 110,
+  'divide': 111,
+  'f1':	112,
+  'f2':	113,
+  'f3':	114,
+  'f4':	115,
+  'f5':	116,
+  'f6':	117,
+  'f7':	118,
+  'f8':	119,
+  'f9':	120,
+  'f10': 121,
+  'f11': 122,
+  'f12': 123,
+  'num lock': 144,
+  'scroll lock': 145,
+  'semi-colon': 186,
+  'equal sign': 187,
+  'comma':	188,
+  'dash': 189,
+  'period': 190,
+  'forward slash': 191,
+  'grave accent': 192,
+  'open bracket': 219,
+  'back slash': 220,
+  'close braket': 221,
+  'single quote': 222
+};
+
+function getKeyByValue(value) {
+  return Object.keys(keymap).find(key => keymap[key] === value);
+}
 
 
 var shinyKeyboardBinding = new Shiny.InputBinding();
@@ -74,7 +110,7 @@ $.extend(shinyKeyboardBinding, {
   // find the dom element with input$id
   // this becomes el downstream
   find: function find(scope) {
-    return $(scope).find(".shinykeyboard")
+    return $(scope).find(".shinykeyboard");
   },
 
   // get the ids of the element with class selected
@@ -100,7 +136,7 @@ $.extend(shinyKeyboardBinding, {
       // but remove the class if they clicked on an already selected class
       if ($("#" + $(el).attr("id") + "_switch").is(':checked')) {
         if ($(evt.target).hasClass("selected")) {
-          $(evt.target).removeClass("selected")
+          $(evt.target).removeClass("selected");
         } else {
           $(evt.target).addClass('selected');
         }
@@ -108,18 +144,15 @@ $.extend(shinyKeyboardBinding, {
         $(el).find(".selected").removeClass("selected");
         $(evt.target).addClass('selected');
       }
-      callback()
+      callback();
     });
     $(document).keyup(function (evt) {
-
-      // 1. GET KEYCODE ARRAY
-      pressedKeys = [];
-      var key = pressedKeys[evt.keyCode]
-
       // 2. CONVERT KEYCODE TO KEY NAME
-      let key_down =
+      let key_down = getKeyByValue(evt.keyCode);
+      console.log(evt.keyCode)
+      console.log(key_down)
       $("#" + key_down).addClass('selected');
-      callback(true)
+      callback(true);
     });
     // 4. USE KEYDOWN TO CLEAR ARRAY
     //    AND CLEAR SELECTED CLASSES?
@@ -134,4 +167,3 @@ $.extend(shinyKeyboardBinding, {
 });
 
 Shiny.inputBindings.register(shinyKeyboardBinding, 'shinykeyboard.keyboardInput');
-}
