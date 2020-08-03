bbcc6a9322ecde2523192e865fe61ce7bb1086a2
Maya Gans <jaffe.maya@gmail.com>
2020-08-02 18:02:02 -0700

    initial keymap

--- inst/js/shinykeyboard.js
+++ inst/js/shinykeyboard.js
@@ -1,3 +1,73 @@
+let keymap = [
+  {backspace: 8},
+  {tab: 9},
+  {enter:	13},
+  {shift:	16},
+  {ctrl:	17},
+  {alt:	18},
+  {pause:	19},
+  {'caps lock': 20},
+  {escape: 27},
+  {'page up':	33},
+  {page down:	34},
+  {end:	35},
+  {home: 36},
+  {'left arrow': 37},
+  {'up arrow':	38},
+  {'right arrow':	39},
+  {'down arrow':	40},
+  {insert:	45},
+  {delete: 46},
+  {0:	48},
+  {1:	49},
+  {2:	50},
+  {3:	51},
+  {4:	52},
+  {5:	53},
+  {6:	54},
+  {7:	55},
+  {8:	56},
+  {9:	57},
+  {a:	65},
+  {b:	66},
+  {c:	67},
+  {d:	68},
+  {e:	69},
+  {f:	70},
+  {g:	71},
+  {h:	72},
+  {i:	73},
+  {j:	74},
+  {k:	75},
+  {l:	76},
+  {m:	77},
+  {n:	78},
+  {o:	79},
+  {p:	80},
+  {q:	81},
+  {r:	82},
+  {s:	83},
+  {t:	84},
+  {u:	85},
+  {v:	86},
+  {w:	87},
+  {x:	88},
+  {y:	89},
+  {z:	90},
+  {"left window key": 91},
+  {"right window key": 92},
+  {"select key":	93},
+  {"numpad 0":	96},
+  {"numpad 1":	97},
+  {"numpad 2":	98},
+  {"numpad 3":	99},
+  {"numpad 4":	100},
+  {"numpad 5":	101},
+  {"numpad 6":	102},
+  {"numpad 7":	103}
+]
+
+
 var shinyKeyboardBinding = new Shiny.InputBinding();
 $.extend(shinyKeyboardBinding, {
 
@@ -41,21 +111,13 @@ $.extend(shinyKeyboardBinding, {
       callback()
     });
     $(document).keyup(function (evt) {
+
       // 1. GET KEYCODE ARRAY
       pressedKeys = [];
-      // while this records multiple keys (YAY)
-      // I don't know how to store them as an array
       var key = pressedKeys[evt.keyCode]
 
       // 2. CONVERT KEYCODE TO KEY NAME
-      // eventually this needs to return from a look up table
-      // to include for non-alphabetic keys
-      // but lets get this working for multiple letters first
-      let key_down = String.fromCharCode(evt.keyCode).toLowerCase()
-
-      // 3. ADD SELECTED CLASS TO ALL ELEMENTS IN ARRAY
-      // add class "selected" to all elements
-      // in the pressedKeys vector
+      let key_down =
       $("#" + key_down).addClass('selected');
       callback(true)
     });
@@ -72,3 +134,4 @@ $.extend(shinyKeyboardBinding, {
 });
 
 Shiny.inputBindings.register(shinyKeyboardBinding, 'shinykeyboard.keyboardInput');
+}
